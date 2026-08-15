df2dbf2bf4039c9bf8600c36fd78cb617011a99b
dopesthetic <150394396+tyudope@users.noreply.github.com>
2026-08-15 23:29:49 +0200

    feat: Book's CRUD is done

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi import status
 from sqlalchemy.orm import Session
 
 
@@ -30,3 +31,41 @@ def read_books(db:Session = Depends(get_db)):
 
     books = db.scalars(select(Book)).all()
     return books
+
+
+@router.get("/{book_id}", response_model=BookRead)
+def read_book_by_id(book_id:int, db:Session = Depends(get_db)):
+
+    book = db.get(Book, book_id)
+    if book is None:
+        return HTTPException(status_code=404, detail = "Book not found.")
+    return book
+
+
+@router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_book_by_id(book_id:int, db:Session=Depends(get_db)):
+
+    book = db.get(Book, book_id)
+    if book is None:
+        raise HTTPException(status_code=404, detail="Book not found.")
+
+    db.delete(book)
+    db.commit()
+    return None
+
+
+@router.patch("/{book_id}", response_model=BookRead)
+def update_book(book_id:int, payload:BookUpdate, db:Session = Depends(get_db)):
+    book = db.get(Book, book_id)
+
+    if book is None:
+        raise HTTPException(status_code=404, detail = "Book not found.")
+
+    updates = payload.model_dump(exclude_unset=True)
+
+    for field, value in updates.items():
+        setattr(book, field, value)
+
+    db.commit()
+    db.refresh(book)
+    return book
